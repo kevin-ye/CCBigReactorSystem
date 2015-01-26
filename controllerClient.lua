995bb57aa3f86a17699cd129cc7e27a0462e1317
kevin-ye <kevin.ye32@gmail.com>
2015-01-25 23:44:12 -0500

    using Touchpoint.lua API

--- controllerClient.lua
+++ controllerClient.lua
@@ -1,6 +1,10 @@
-os.loadAPI("gui")
+os.loadAPI("Touchpoint")
 
 ---- global
+
+extra = {
+	eventUpdateDelay = 0.5  --- update every tick
+}
 
 pkgreciever = {
 	listeningPort = 1100
@@ -13,21 +17,105 @@ pkgsender = {
 	end
 }
 
-reactors = {rserver1}
+reactors = {}
 
-rserver1 = {
-    listeningPort = 1101,
+rserver = {
     id = "Main Reactor",
-	maxEnergy = function (self)
+	getEnergyStored = function (self, port)
                     local newpkg = {
-                        request = "getmaxEnergy"
+                        request = "getEnergyStored"
                     }
-                    pkgsender:send(self.listeningPort, newpkg)
+                    pkgsender:send(port, newpkg)
 	            end,
+	isActive = function (self, port)
+                    local newpkg = {
+                        request = "getActive"
+                    }
+                    pkgsender:send(port, newpkg)
+	            end,
+	getFuelTemperature = function (self, port)
+                    local newpkg = {
+                        request = "getFuelTemperature"
+                    }
+                    pkgsender:send(port, newpkg)
+	            end,
+	getCasingTemperature = function (self, port)
+                    local newpkg = {
+                        request = "getCasingTemperature"
+                    }
+                    pkgsender:send(port, newpkg)
+	            end,
+	getFuelAmount = function (self, port)
+                    local newpkg = {
+                        request = "getFuelAmount"
+                    }
+                    pkgsender:send(port, newpkg)
+	            end,
+	getFuelAmountMax = function (self, port)
+                    local newpkg = {
+                        request = "getFuelAmountMax"
+                    }
+                    pkgsender:send(port, newpkg)
+	            end,
+	getEnergyProducedLastTick = function (self, port)
+                    local newpkg = {
+                        request = "getEnergyProducedLastTick"
+                    }
+                    pkgsender:send(port, newpkg)
+	            end,
+	getFuelReactivity = function (self, port)
+                    local newpkg = {
+                        request = "getFuelReactivity"
+                    }
+                    pkgsender:send(port, newpkg)
+	            end,
+	getFuelConsumedLastTick = function (self, port)
+                    local newpkg = {
+                        request = "getFuelConsumedLastTick"
+                    }
+                    pkgsender:send(port, newpkg)
+	            end,
+	setActive = function (self, port, active)
+	                local newpkg = {
+                        request = "setActive",
+                        value = active
+                    }
+                    pkgsender:send(port, newpkg)
+	            end
 }
 
----- main 
+---- views
+currentReactor = {
+	updateView = function (self, info)
 
+	             end
+
+}
+
+---- model
+model = {
+	views = {}.
+	addView = function (self, name, v)
+	              views[name] = v;
+	          end,
+	notifyAllViews = function (self, info)
+	                     for v in self.views do
+	                     	v.updateView(info)
+	                     end
+	                 end
+}
+
+
+---- main 
+t = touchpoint.new("top")
+reactors["r1"] = 1101;   ---- added reactor server 1 with port 1101
 
 
 ---- event loop
+while (true) do
+	local event, args1, args2, args3 = os.pullEvent()
+	if event == "monitor_touch" then 
+		---- button click
+	else
+	sleep(extra.eventUpdateDelay)
+end
